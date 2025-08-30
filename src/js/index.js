02d5502ffd7f27a607dfbdad3fca65ea06da61db
gabsdv <gs44@illinois.edu>
2025-08-30 14:16:54 -0500

    done

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
-/* Your JS here. */
-console.log('Hello World!')
+function changeColor(color) {
+    document.getElementById('card').style.backgroundColor = color;
+}
+
+function closeCard() {
+    document.getElementById('inner-card').style.display = 'none';
+}
+
+function openCard() {
+    document.getElementById('inner-card').style.display = 'flex';
+}
+
+
+document.getElementById('img').addEventListener('click', openCard);
+document.getElementById('name').addEventListener('click', closeCard);
+document.querySelectorAll('button').forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const color = e.target.textContent.toLowerCase();
+        changeColor(color);
+    })
+});
